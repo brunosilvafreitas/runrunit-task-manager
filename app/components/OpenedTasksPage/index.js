29ab5fdd117fbb4079c41ff63762396e9934b6a5
Bruno Freitas <bsfreitas@uolinc.com>
2017-08-23 14:34:22 -0300

    Add play and pause tasks

--- app/components/OpenedTasksPage/index.js
+++ app/components/OpenedTasksPage/index.js
@@ -9,9 +9,35 @@ class OpenedTasksPage extends React.Component {
     this.state = {
       tasks: []
     };
+
+    this.handleGetList = this.handleGetList.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handlePause = this.handlePause.bind(this);
   }
 
   componentDidMount() {
+    this.handleGetList();
+  }
+
+  handlePlay(id) {
+    return () => {
+      request.post(`https://secure.runrun.it/api/v1.0/tasks/${id}/play`)
+        .then(response => {
+          this.handleGetList();
+        });
+    };
+  }
+
+  handlePause(id) {
+    return () => {
+      request.post(`https://secure.runrun.it/api/v1.0/tasks/${id}/pause`)
+        .then(response => {
+          this.handleGetList();
+        });
+    };
+  }
+
+  handleGetList() {
     request.get('https://secure.runrun.it/api/v1.0/tasks', {
       params: {
         user_id: localStorage.getItem("userid"),
@@ -29,10 +55,10 @@ class OpenedTasksPage extends React.Component {
         {task.id} - {task.title}
         <div>
           {(task.is_working_on) ?
-            (<button type="button" className="btn btn-sm btn-primary">
+            (<button type="button" className="btn btn-sm btn-primary" onClick={this.handlePause(task.id)}>
             <span className="oi" data-glyph="media-pause"></span> PAUSE
             </button>) :
-            (<button type="button" className="btn btn-sm btn-primary">
+            (<button type="button" className="btn btn-sm btn-primary" onClick={this.handlePlay(task.id)}>
             <span className="oi" data-glyph="media-play"></span> WORK
             </button>)
           } <button type="button" className="btn btn-sm btn-light">COMPLETE</button>
